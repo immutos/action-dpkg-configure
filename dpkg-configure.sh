a2f8fc12937191f275d84defd98ce9ef19124d0c
Damian Peckett <damian@pecke.tt>
2024-11-02 21:55:46 +0100

    feat: write the logs as usual

--- dpkg-configure.sh
+++ dpkg-configure.sh
@@ -2,4 +2,9 @@
 set -e
 
 # Configure all unpacked packages.
-dpkg --root=/rootfs --admindir=/rootfs/var/lib/dpkg --instdir=/rootfs --configure -a
+dpkg \
+  --root=/rootfs \
+  --admindir=/rootfs/var/lib/dpkg \
+  --instdir=/rootfs \
+  --log=/rootfs/var/log/dpkg.log \
+  --configure -a
